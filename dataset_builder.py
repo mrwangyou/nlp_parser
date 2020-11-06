5f368a99cd77df5fff1f825cfdfb40b23bd0df8e
wangningnan <mrwangyou@qq.com>
2020-11-06 11:28:42 +0800

    Finished

--- dataset_builder.py
+++ dataset_builder.py
@@ -13,7 +13,7 @@ for key, char in enumerate(dict):
     f.write(str(key) + ' ' + char + '\n')
 f.close()
 
-f = open('./dataset/2014_corpus_single_word_dict.txt', 'w', encoding='UTF-8')
+f = open('./dataset/2014_corpus_single_word_dict.txt', 'r', encoding='UTF-8')
 for line in f:
     key, char = line.split()
     dict[char] = key
@@ -21,8 +21,25 @@ f.close()
 
 f = open('./dataset/2014_corpus_single_word.txt', 'r', encoding='UTF-8')
 num = 0
+flag = 0
 dataset = []
 for sent in f:
-
+    sentSet = []
     num = num + 1
     for char in sent.split():
+        sentSet.append(int(dict[char]))
+    dataset.append(sentSet)
+    if num >= 100:
+        finalSet = []
+        maxSize = 0
+        for line in dataset:
+            maxSize = max(maxSize, len(line))
+        for line in dataset:
+            tmp = line
+            while len(tmp) < maxSize:
+                tmp.append(len(dict))
+            finalSet.append(tmp)
+        torch.save(torch.tensor(dataset), './dataset/word_dict_tensor/' + str(flag) + '.pth')
+        flag = flag + 1
+        dataset = []
+torch.save(torch.tensor(dataset), './dataset/word_dict_tensor/' + str(flag) + '.pth')
